5c8775cdbf1a01ea1a3fa1c7e6f6133b3864db34
Eric Christenson <ebakunin@gmail.com>
2015-02-22 19:58:20 -0700

    Improved code, added minified version and example.

--- jquery.fn.visibilityFadeIn.js
+++ jquery.fn.visibilityFadeIn.js
@@ -1,20 +1,22 @@
 ﻿/**
- * A version of jQuery.fadeIn() that uses 'visibility' CSS styling
- * 
- * @author   Eric Christenson
- * @license  GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * A version of jQuery.fadeIn() that uses 'visibility' CSS styling.
+ * Note that the element must start with visibility: hidden
  *
- * @param  int  delay (fade in time in milliseconds. Optional)
- * @return void
+ * @author   Eric Christenson
+ * @date     February 22, 2015
+ * @version  2.0
+ * @license  MIT License (http://opensource.org/licenses/MIT)
+ *
+ * @param  fadeSpeed  int (time in milliseconds. Optional, defaults to 100)
  */
-(function($) {
-	$.fn.visibilityFadeIn = function(delay) {
-		if (undefined === delay) {
-			delay = 100;
-		}
+(function ($) {
+    $.fn.visibilityFadeIn = function (fadeSpeed) {
+        fadeSpeed = fadeSpeed || 1000;
 
-		$(this).each(function(index) {
-			$(this).fadeTo(0,0).delay(index*100).css('visibility','visible').fadeTo('fast',1);
-		});
-	}
+        $(this).each(function (index) {
+            if ($(this).css('visibility') === 'hidden') {
+                $(this).delay(index * 100).css({opacity: 0, visibility: 'visible'}).fadeTo(parseInt(fadeSpeed), 1);
+            }
+        });
+    }
 })(jQuery);
