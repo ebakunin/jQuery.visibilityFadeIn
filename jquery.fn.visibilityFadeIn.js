25e3f17263601aec7eade9093d55a9d698fafeca
Eric Christenson <ebakunin@gmail.com>
2015-03-11 01:08:58 -0700

    Added complete option. Changed duration to match $.fadeIn() parameter.

--- jquery.fn.visibilityFadeIn.js
+++ jquery.fn.visibilityFadeIn.js
@@ -4,19 +4,35 @@
  *
  * @author   Eric Christenson
  * @date     February 22, 2015
- * @version  2.0
+ * @version  3.0
  * @license  MIT License (http://opensource.org/licenses/MIT)
  *
- * @param  fadeSpeed  int (time in milliseconds. Optional, defaults to 100)
+ * @param  duration  mixed (optional. Accepts time in milliseconds, 'slow', or 'fast')
+ * @param  complete  function (optional. A callback function)
  */
 (function ($) {
-    $.fn.visibilityFadeIn = function (fadeSpeed) {
-        fadeSpeed = fadeSpeed || 1000;
+    $.fn.visibilityFadeIn = function (duration, complete) {
+        var speed = duration || 400;
+
+        switch (speed) {
+            case 'fast':
+                speed = 200;
+                break;
+            case 'slow':
+                speed = 600;
+                break;
+            default:
+                speed = parseInt(duration);
+        }
 
         $(this).each(function (index) {
             if ($(this).css('visibility') === 'hidden') {
-                $(this).delay(index * 100).css({opacity: 0, visibility: 'visible'}).fadeTo(parseInt(fadeSpeed), 1);
+                $(this).delay(index * 100).css({ opacity: 0, visibility: 'visible' }).fadeTo(speed, 1);
             }
         });
-    }
+
+        $(this).promise().done(function () {
+            complete();
+        });
+    };
 })(jQuery);
